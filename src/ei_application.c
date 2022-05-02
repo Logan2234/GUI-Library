8bc135f792372a12810fe72b377a4f823cd68d27
Logan Willem <logan.willem@grenoble-inp.org>
2022-05-03 00:37:30 +0200

    Etape 1A done

--- src/ei_application.c
+++ src/ei_application.c
@@ -9,3 +9,18 @@ void ei_app_create(ei_size_t main_window_size, ei_bool_t fullscreen)
     const ei_surface_t racine = hw_create_window(main_window_size, fullscreen);
     ei_surface_t offscreen = hw_surface_create(racine, main_window_size, EI_TRUE);
 }
+
+void ei_app_run()
+{
+    getchar();
+}
+
+void ei_app_free()
+{
+    
+}
+
+ei_widget_t* ei_app_root_widget(void)
+{
+    return NULL;
+}
