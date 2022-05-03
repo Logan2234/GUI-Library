e6299069c9e9108fa2f5872c9e5b12c9a2982a59
Jorge <jorge.luri-vano@grenoble-inp.org>
2022-05-03 09:45:22 +0200

    Premiers pas du premier programme

---
+++ src/ei_application.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "../include/ei_application.h"
+#include "../include/ei_widgetclass.h"
+#include <stdint.h>
+#include <string.h>
+
+void ei_app_create(ei_size_t main_window_size, ei_bool_t fullscreen) {
+    hw_init();
+    ei_widgetclass_register()
+}
