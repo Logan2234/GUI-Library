b65413dfece89ab69f4e683341a833194b16859e
Jorge Luri Vano <jorge.luri-vano@grenoble-inp.org>
2022-05-03 07:50:47 +0000

    On récup ce qu'on a effacé

---
+++ src/et_application.c
@@ -0,0 +1,26 @@
+#include "ei_application.h"
+#include "ei_geometrymanager.h"
+
+void ei_app_create(ei_size_t main_window_size, ei_bool_t fullscreen)
+{
+    hw_init();
+    // ei_widgetclass_register(widget_class); A faire pour toutes les classes de widget
+    // ei_geometrymanager_register();
+    const ei_surface_t racine = hw_create_window(main_window_size, fullscreen);
+    ei_surface_t offscreen = hw_surface_create(racine, main_window_size, EI_TRUE);
+}
+
+void ei_app_run()
+{
+    getchar();
+}
+
+void ei_app_free()
+{
+    
+}
+
+ei_widget_t* ei_app_root_widget(void)
+{
+    return NULL;
+}
