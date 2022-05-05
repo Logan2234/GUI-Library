e4214abe4d90c3a2da369a189f679b62778d858b
Jorge <jorge.luri-vano@grenoble-inp.org>
2022-05-05 13:52:34 +0200

    code de ei_draw_arc

--- src/ei_draw_auxiliaire.c
+++ src/ei_draw_auxiliaire.c
@@ -1,12 +1,35 @@
 #include "ei_draw.h"
 #include "hw_interface.h"
 #include "ei_autre_draw.h"
+#include <math.h>
+#include <stdlib.h>
 
 
 ei_linked_point_t* ei_draw_arc (ei_point_t center,
-                                int rayon,
+                                uint32_t rayon,
                                 float angle_debut,
                                 float angle_fin)
 {
-    // TODO
+    float longueur_arc = abs((float) rayon * (angle_fin - angle_debut));
+    float pas = (angle_fin - angle_debut) / longueur_arc;
+    ei_linked_point_t *liste_point;
+    liste_point->point.x = (int) rayon * cos(angle_debut);
+    liste_point->point.y = (int) rayon * sin(angle_debut);
+    ei_linked_point_t *suivant;
+    suivant = liste_point;
+    for (i = 1; i <= longueur_arc; i++){
+        int x = (int) rayon * cos(angle_debut + i * pas);
+        int y = (int) rayon * sin(angle_debut + i * pas);
+        while (suivant->next != NULL){
+            suivant = suivant->next;
+        }
+        if (suivant->next->point.x != x || suivant->next->point.y != y){
+            ei_linked_point_t *nouveau;
+            nouveau->point.x = x;
+            nouveau->point.y = y;
+            nouveau->next = NULL;
+            suivant->next = nouveau;
+        }
+    }
+    return liste_point;
 }
