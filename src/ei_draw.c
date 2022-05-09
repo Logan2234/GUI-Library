1e68ea6d3b27cb294d988d43fcf6e25c31ce4d75
Logan Willem <logan.willem@grenoble-inp.org>
2022-05-09 15:25:01 +0200

    Ajout des fonctions dans le .c

--- src/ei_draw.c
+++ src/ei_draw.c
@@ -76,3 +76,15 @@ void ei_fill(ei_surface_t surface,
         }
     }
 }
+
+void ei_draw_text(ei_surface_t surface, const ei_point_t *where,
+                  const char *text, ei_font_t font,
+                  ei_color_t color, const ei_rect_t *clipper)
+{
+}
+
+int ei_copy_surface(ei_surface_t destination, const ei_rect_t *dst_rect,
+                    ei_surface_t source, const ei_rect_t *src_rect, ei_bool_t alpha)
+{
+    
+}
