ff4b23a9f84024276d96f1a93f2ffba19382c9a7
Jorge <jorge.luri-vano@grenoble-inp.org>
2022-05-10 17:16:50 +0200

    Debug de ei_bind

--- src/ei_event.c
+++ src/ei_event.c
@@ -77,7 +77,7 @@ void ei_unbind(ei_eventtype_t eventtype, ei_widget_t *widget, ei_tag_t tag, ei_c
         {
             struct liste_events_widgets *suivant = liste_widget;
             struct liste_events_widgets *ancien = liste_widget;
-            if (suivant->widget->pick_id == widget->pick_id && suivant->callback == callback &&
+            if ((suivant->widget->pick_id == widget->pick_id || (suivant->widget == NULL && widget == NULL)) && suivant->callback == callback &&
                 suivant->user_param == user_param && suivant->eventtype == eventtype)
             {
                 liste_widget = liste_widget->next;
@@ -100,7 +100,7 @@ void ei_unbind(ei_eventtype_t eventtype, ei_widget_t *widget, ei_tag_t tag, ei_c
                     suivant = suivant->next;
                 }
             }
-            if (suivant->widget->pick_id == widget->pick_id && suivant->callback == callback &&
+            if ((suivant->widget->pick_id == widget->pick_id || (suivant->widget == NULL && widget == NULL)) && suivant->callback == callback &&
                 suivant->user_param == user_param && suivant->eventtype == eventtype)
             {
                 suivant = ancien->next;
