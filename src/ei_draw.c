d02e88e6e96e92b03d20849f4511727a8fc825eb
Nils DEPUILLE <nils.depuille@grenoble-inp.org>
2022-05-13 17:34:42 +0200

    alpha

--- src/ei_draw.c
+++ src/ei_draw.c
@@ -119,28 +119,35 @@ int ei_copy_surface(ei_surface_t destination, const ei_rect_t *dst_rect,
         uint32_t *last_pixel_of_current_line_dest = origine_dest + (dst_rect->top_left.y + 1) * dst_rect->size.width;
 
         uint32_t last_value_of_j = 0;
-
+        int32_t logan = 0;
         for (uint32_t i = 0; i < src_rect->size.height; i++)
         {
             /* On dessine toutes la partie rectangulaire */
             for (uint32_t j = 0; j < src_rect->size.width; j++)
             {
                 /* On gère le cas où on dépasse la bordure basse de l'écran en arrêtant les deux boucles*/
-                if (pixel_ptr_dest == last_pixel_of_current_line_dest)
-                {
-                    last_value_of_j = j;
-                    break;
-                }
+                // if (pixel_ptr_dest == last_pixel_of_current_line_dest)
+                // {
+                //     last_value_of_j = j;
+                //     break;
+                // }
                 if (alpha == EI_TRUE)
                 {
                     uint8_t *dest = (uint8_t *)pixel_ptr_dest;
                     uint8_t *src = (uint8_t *)pixel_ptr_src;
-
-                    *(dest + ig) = *(src + ia) * *(src + ig) + (255 - *(src + ia)) * *(dest + ig) / 255;
-                    *(dest + ir) = *(src + ia) * *(src + ir) + (255 - *(src + ia)) * *(dest + ir) / 255;
-                    *(dest + ib) = *(src + ia) * *(src + ib) + (255 - *(src + ia)) * *(dest + ib) / 255;
+                    if (*(src + ia) != 0)
+                    {
+                    
+    
+                    *(dest + ig) = *(src + ia) * *(src + ig) + (255 + *(src + ia)) * *(dest + ig) / 255;
+                    *(dest + ir) = *(src + ia) * *(src + ir) + (255 + *(src + ia)) * *(dest + ir) / 255;
+                    *(dest + ib) = *(src + ia) * *(src + ib) + (255 + *(src + ia)) * *(dest + ib) / 255;
+                    }
+                    else {
+                        printf("%i\n", logan++);
+                    }
                     // printf("%d\n", *pixel_ptr_dest);
-                    *pixel_ptr_dest++;
+                    *pixel_ptr_dest++;//= *pixel_ptr_src++;
                     *pixel_ptr_src++;
                 }
                 else
