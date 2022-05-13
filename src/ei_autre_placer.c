9a35202deb5245bf3efbe6ff2b350e8b851abc16
Nils Depuille <nils.depuille@grenoble-inp.org>
2022-05-13 14:32:08 +0200

    rectification de ei_place

--- src/ei_autre_placer.c
+++ src/ei_autre_placer.c
@@ -1,8 +1,82 @@
 #include "ei_geometrymanager.h"
+#include "ei_placer.h"
 
-void placer_runfunc(struct ei_widget_t *widget)
+void placer_runfunc(struct ei_placer_t *placer)
 {
-    // TODO
+    /* Gestion du paramètre geom_params du widget */
+    int width_parent = widget->requested_size.width;
+    int height_parent = widget->requested_size.height;
+    widget->geom_params = ei_geometrymanager_from_name("placer");
+    /* Initialisation des variables pour contrer les NULL */
+    ei_point_t *top_left = calloc(1, sizeof(ei_point_t));
+    if (widget->parent != NULL)
+    {
+        top_left->x = ((placer->rel_x != -1) ? widget->parent->content_rect->top_left.x : 0);
+        top_left->y = ((placer->rel_y != -1) ? widget->parent->content_rect->top_left.y : 0);
+        width_parent = widget->parent->content_rect->size.width;
+        height_parent = widget->parent->content_rect->size.height;
+    }
+    placer->rel_x = ((placer->rel_x == -1) ? 0 : -1);
+    placer->rel_y = ((placer->rel_y == -1) ? 0 : -1);
+    if (placer->anchor == NULL)
+    {
+        top_left->x += x2 + rel_x2 * width_parent;
+        top_left->y += y2 + rel_y2 * height_parent;
+    }
+    else
+    {
+        switch (*placer->anchor)
+        {
+        case ei_anc_none:
+            top_left->x += placer->x + placer->rel_x * width_parent;
+            top_left->y += placer->y + placer->rel_y * height_parent;
+            break;
+        case ei_anc_center:
+            top_left->x += placer->x + placer_rel_x* width_parent - (placer->width + placer->rel_width) / 2;
+            top_left->y += placer->y + placer_rel_y* height_parent - placer->height / 2;
+            break;
+        case ei_anc_north:
+            top_left->x += placer->x + placer->rel_x * width_parent - (placer->width + placer->rel_width) / 2;
+            top_left->y += placer->y + placer->rel_y * height_parent;
+            break;
+        case ei_anc_south:
+            top_left->x += placer->x + placer->rel_x * width_parent - (placer->width + placer->rel_width) / 2;
+            top_left->y += placer->y + placer->rel_y * height_parent - (placer->height + placer->rel_height);
+            break;
+        case ei_anc_east:
+            top_left->x += placer->x + placer->rel_x * width_parent - (placer->width + placer->rel_width);
+            top_left->y += placer->y + placer->rel_y * height_parent - (placer->height + placer->rel_height) / 2;
+            break;
+        case ei_anc_west:
+            top_left->x += placer->x + placer->rel_x * width_parent;
+            top_left->y += placer->y + placer->rel_y * height_parent - (placer->height + placer->rel_height) / 2;
+            break;
+        case ei_anc_northeast:
+            top_left->x += placer->x + placer->rel_x * width_parent - (placer->width + placer->rel_width);
+            top_left->y += placer->y + placer->rel_y * height_parent;
+            break;
+        case ei_anc_northwest:
+            top_left->x += placer->x + placer->rel_x * width_parent;
+            top_left->y += placer->y + placer->rel_y * height_parent;
+            break;
+        case ei_anc_southeast:
+            top_left->x += placer->x + placer->rel_x * width_parent - (placer->width + placer->rel_width);
+            top_left->y += placer->y + placer->rel_y * height_parent - (placer->height + placer->rel_height);
+            break;
+        case ei_anc_southwest:
+            top_left->x += placer->x + placer->rel_x * width_parent;
+            top_left->y += placer->y + placer->rel_y * height_parent - (placer->height + placer->rel_height);
+            break;
+        }
+    }
+
+    /* Maintenant on remplace dans les données de widgets */
+    widget->content_rect->size.width = width2;
+    widget->content_rect->size.height = height2;
+    
+    widget->content_rect->top_left = *top_left;
+    free(top_left);
+
 }
 
 void placer_releasefunc(struct ei_widget_t *widget)
